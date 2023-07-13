a269a071ab64bae18f67328fcd2cad403ea4c1d7
Andreas Nicklaus <andreas_nicklaus@web.de>
2023-07-13 12:48:32 +0200

    filterable calendar

--- app/src/components/calendar/CalendarColorLegend.vue
+++ app/src/components/calendar/CalendarColorLegend.vue
@@ -1,17 +1,22 @@
 <template>
   <b-row class="calendarColorLegend">
-    <span v-for="therapeut in this.therapeuten" :key="therapeut.id">
-      <svg class="m-1">
+    <b-button
+      variant="light"
+      v-for="therapeut in this.therapeuten"
+      :key="therapeut.id"
+      @click="selectTherapeut(therapeut)"
+    >
+      <svg class="mx-1">
         <circle
           cx="20"
           cy="20"
           r="20"
           stroke-width="3"
-          :fill="therapeut.color"
+          :fill="therapeutColor(therapeut)"
         />
       </svg>
       <span>{{ therapeut.name }}</span>
-    </span>
+    </b-button>
   </b-row>
 </template>
 
@@ -23,6 +28,25 @@ export default {
       type: Array,
       required: true,
     },
+    value: {
+      type: Array,
+    },
+  },
+  methods: {
+    therapeutColor(therapeut) {
+      if (this.value.length == 0) return therapeut.color;
+      else return this.value.includes(therapeut.id) ? therapeut.color : "#333";
+    },
+    selectTherapeut(therapeut) {
+      if (this.value.includes(therapeut.id))
+        this.$emit(
+          "input",
+          this.value.filter((t) => t != therapeut.id)
+        );
+      else {
+        this.$emit("input", [...this.value, therapeut.id]);
+      }
+    },
   },
 };
 </script>
@@ -31,5 +55,9 @@ export default {
 svg:not(.bi) {
   height: 40px;
   width: 40px;
+
+  &:hover {
+    cursor: pointer;
+  }
 }
 </style>
